127456b6cae8c63b51b8e42a1bbfebcaf127ae69
vagrant <Dcameroncheng@gmail.com>
2020-04-17 20:11:12 +0000

    add getUserByEmail function into post login

--- helpers.js
+++ helpers.js
@@ -1,10 +1,11 @@
 const getUserByEmail = function(email, database) {
   for (let key in database) {
     let user = database[key];
-    if (users.email === email) {
-      return users[user];
+    if (user.email === email) {
+      return user;
     }
   }
+  return false;
 };
 
 
